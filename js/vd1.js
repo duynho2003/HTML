729e3f8f37cadb4ff7e7db575eda81896dbf2ed3
duynho2003 <khenchaynguyen@gmail.com>
2022-09-23 17:18:30 +0700

    Sep 23 2022

---
+++ js/vd1.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#hinh-ma").hover(function (){
+        $(this).attr("src", "images/hero_iphone_14_pro__kzr001ge0262_large.jpg");
+    }, function () {
+        $(this).attr("src", "images/hero_iphone_14_pro__kzr001ge0262_large.jpg");
+    });
+});
